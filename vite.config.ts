abae83635d6acee62b07aa12574d534af788c3f2
Cloyd Lau <31238760+cloydlau@users.noreply.github.com>
2023-11-10 10:25:39 +0800

    build: add rollup-plugin-visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import type { ConfigEnv, UserConfigExport } from 'vite'
+import { visualizer } from 'rollup-plugin-visualizer'
 import { PascalCasedName, name } from './package.json'
 
 // https://vitejs.dev/config/
 export default ({ command }: ConfigEnv): UserConfigExport => {
   return {
+    plugins: [
+      { ...visualizer(), apply: 'build' },
+    ],
     build: {
       lib: {
         name,
