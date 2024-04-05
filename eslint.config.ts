bfda7924f52b4919ce29b085a3f880f128c442fc
Cloyd Lau <31238760+cloydlau@users.noreply.github.com>
2024-04-06 00:48:08 +0800

    chore: modify ESLint rules

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,20 +3,23 @@ import antfu from '@antfu/eslint-config'
 export default antfu(
   {
     formatters: true,
-    ignores: ['demo/', 'src/'],
+    ignores: ['src/']
   },
   {
     rules: {
+      'brace-style': ['error', 'stroustrup', { allowSingleLine: false }],
       'curly': ['error', 'all'],
       'no-console': 'off',
       'vue/attribute-hyphenation': 'off',
       'vue/custom-event-name-casing': 'off',
       'vue/max-attributes-per-line': 'error',
+      'vue/multi-word-component-names': 'off',
       'vue/no-deprecated-v-bind-sync': 'off',
       'vue/no-deprecated-v-on-native-modifier': 'off',
       'vue/no-deprecated-destroyed-lifecycle': 'off',
       'vue/no-deprecated-dollar-listeners-api': 'off',
       'vue/no-deprecated-dollar-scopedslots-api': 'off',
+      'vue/no-v-html': 'off',
       'vue/singleline-html-element-content-newline': 'off',
     },
   },
